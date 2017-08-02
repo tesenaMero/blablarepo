7b0af4dde31d317b0dbb61f0ad9706df5356eedf
leind <danielcardeenas@gmail.com>
2017-08-02 17:59:41 -0500

    Almost finished orders page

--- src/cemex/ClientApp/app/components/orders-table/orders-table.component.ts
+++ src/cemex/ClientApp/app/components/orders-table/orders-table.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-orders-table',
   templateUrl: './orders-table.html',
-  styleUrls: ['./orders-table.scss']
+  styleUrls: ['./orders-table.scss', './orders-table.specific.scss']
 })
 export class OrdersTableComponent implements OnInit {
 
