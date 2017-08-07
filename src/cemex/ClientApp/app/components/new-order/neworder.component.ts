a5aa8e4e92212cb3b7b6e2f64029a7177f851ac3
Edwin Alejandro Esquivel Soto <edwin.esquivel@neoris.cxnetworks.net>
2017-08-06 22:23:24 -0500

    Add new order without cellphone layout

---
+++ src/cemex/ClientApp/app/components/new-order/neworder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'page-neworder',
+  templateUrl: './neworder.html',
+  styleUrls: ['./neworder.scss']
+})
+export class NewOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
