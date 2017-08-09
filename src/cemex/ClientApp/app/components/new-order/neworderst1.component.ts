296f5cb3a729aa20c9be47a972742b9225f2504a
Edwin Alejandro Esquivel Soto <edwin.esquivel@neoris.cxnetworks.net>
2017-08-09 13:13:03 -0500

    removed merge conflicts with DCM008CX1295-1531-fe-create-shopping-car

---
+++ src/cemex/ClientApp/app/components/new-order/neworderst1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'page-neworder',
+  templateUrl: './neworderst1.html',
+  styleUrls: ['./neworderst1.scss']
+})
+export class NewOrderComponentSt1 implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
