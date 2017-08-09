04e91efd843f1a337c79cfd3198c95b581ef2b69
Edwin Alejandro Esquivel Soto <edwin.esquivel@neoris.cxnetworks.net>
2017-08-09 13:19:17 -0500

    removed merge conflicts with DCM008CX1295-1531-fe-create-shopping-car

--- src/cemex/ClientApp/app/app.module.shared.ts
+++ src/cemex/ClientApp/app/app.module.shared.ts
@@ -7,6 +7,7 @@ import { OrdersComponent } from './components/orders/orders.component'
 import { OrdersTableComponent } from './components/orders-table/orders-table.component'
 import { NewOrderComponent } from './components/new-order/new-order.component'
 import { CartComponent } from './components/cart/cart.component';
+import { NewOrderComponentSt1 } from './components/new-order/neworderst1.component'
 
 export const sharedConfig: NgModule = {
     bootstrap: [ AppComponent ],
@@ -17,6 +18,7 @@ export const sharedConfig: NgModule = {
         OrdersComponent,
         NewOrderComponent,
         CartComponent,
+	NewOrderComponentSt1,
 
         // Regular components
         OrdersTableComponent
@@ -25,13 +27,13 @@ export const sharedConfig: NgModule = {
         RouterModule.forRoot([
             { path: '', redirectTo: 'app', pathMatch: 'full' },
             { path: 'orders', redirectTo:'app', pathMatch:'full' },
-            { path: 'app', component:  DashboardComponent, 
-                children: [
-                    { path: '', redirectTo: 'orders', pathMatch:'full' },
-                    { path: 'orders', component: OrdersComponent },
-                    { path: 'new', component: NewOrderComponent },
-                    { path: 'cart', component: CartComponent },
-                ]
+            { path: 'app', component:  DashboardComponent, children: [
+                	{ path: '', redirectTo: 'orders', pathMatch:'full' },
+		        { path: 'orders', component: OrdersComponent },
+		        { path: 'newst1', component: NewOrderComponentSt1 },		    
+	        	{ path: 'new', component: NewOrderComponent },
+		        { path: 'cart', component: CartComponent },
+		]
             },
             { path: '**', redirectTo: 'app' }
         ])
