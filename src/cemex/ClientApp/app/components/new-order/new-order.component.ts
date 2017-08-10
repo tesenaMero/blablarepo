c18566c902fbd3c1d0edb268f186ffe9319e280b
leind <danielcardeenas@gmail.com>
2017-08-10 11:24:55 -0500

    Added google maps loader

--- src/cemex/ClientApp/app/components/new-order/new-order.component.ts
+++ src/cemex/ClientApp/app/components/new-order/new-order.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { GoogleMapsHelper } from '../../utils/googlemaps.helper'
+//import { } from '@types/googlemaps';
+//declare var google: any;
 
 @Component({
-  selector: 'new-order',
-  templateUrl: './new-order.html',
-  styleUrls: ['./new-order.scss']
+    selector: 'new-order',
+    templateUrl: './new-order.html',
+    styleUrls: ['./new-order.scss']
 })
 export class NewOrderComponent implements OnInit {
-  constructor() { }
+    @Input() mapOptions?: google.maps.MapOptions;
 
-  ngOnInit() {
-  }
+    map: any; // Map instance
+
+    constructor() { }
+
+    ngOnInit() {
+        GoogleMapsHelper.lazyLoadMap("map", (map) => {
+            this.map = map;
+            map.setOptions({ zoom: 14, center: { lat: 50.077626, lng: 14.424686 } });
+        });
+    }
 }
---
+++ src/cemex/ClientApp/app/utils/googlemaps.helper.ts
@@ -0,0 +1,50 @@
+/**
+ * Google maps helper
+ * Lazy loads the map, exposes the entire Javascript API via @types
+ * 
+ * @param: string: The <div> id
+ * @callback: (): map instance
+ * 
+ * Example:
+ * GoogleMapsHelper.lazyLoadMap("map", (map) => {
+ *     map.setOptions(GoogleMapsHelper.mapOptions);
+ *     var marker = new google.maps.Marker({
+ *         position: { lat: -25.363, lng: 131.044 },
+ *         map: map
+ *     });
+ * });
+ */
+
+import { } from '@types/googlemaps';
+
+export class GoogleMapsHelper {
+    private static src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCpk_GAvrtS8yi-y6Dx8k5hltS18uxjUPI"
+    private static scriptId = "googlemapsapi"
+
+    static lazyLoadMap(id = "map", callback: (map) => any) : void {
+        if (!document.getElementById(this.scriptId)) {
+            // Appends script into html
+            document.body.appendChild(Object.assign(document.createElement('script'), {
+                type: 'text/javascript',
+                id: this.scriptId,
+                src: this.src,
+                onload: () => {
+                    let map = new google.maps.Map(document.getElementById(id));
+                    callback(map);
+                }
+            }));
+        }
+        else {
+            // Script already added
+            console.log("Already loaded script")
+            let map = new google.maps.Map(document.getElementById(id));
+            callback(map);
+        }
+    }
+
+    // Google map options
+    static readonly mapOptions: google.maps.MapOptions = {
+        zoom: 4,
+        center: { lat: -25.363, lng: 131.044 },
+    };
+}
