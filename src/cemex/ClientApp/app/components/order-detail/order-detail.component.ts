a4efe005cad1f4cf16e42de724461e4af0228892
Edwin Alejandro Esquivel Soto <edwin.esquivel@neoris.cxnetworks.net>
2017-08-23 19:16:09 +0200

    add tab comments in order-detail

--- src/cemex/ClientApp/app/components/order-detail/order-detail.component.ts
+++ src/cemex/ClientApp/app/components/order-detail/order-detail.component.ts
@@ -11,9 +11,29 @@ import { Component, OnInit, Input } from '@angular/core';
 export class OrderDetailComponent implements OnInit {
     order = 7543189;
     requestDate = "31/12/2017, 15:00 - 16:00";
-
-    constructor() { }
+    @Input() show: boolean = false;
+    constructor() { 
+    }
 
     ngOnInit() {
     }
+
+
+    changeTabComments(){
+        var elem = document.getElementById('products-id').style.display = 'none';
+        document.getElementById("products").classList.remove('active');
+        document.getElementById("comments").classList.add('active');  
+        this.show = true;
+    }
+/* Waiting for invision    
+    changeTabAction(){
+    }     
+*/    
+    changeTabProducts(){
+        var elem = document.getElementById('products-id').style.display = 'inline-flex';
+        document.getElementById("comments").classList.remove('active');
+        document.getElementById("products").classList.add('active'); 
+        this.show = false;
+    } 
+
 }
