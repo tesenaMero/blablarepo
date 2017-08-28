00823fc5ed9f2e7115f1dfb150287a3f13f88a5e
leind <danielcardeenas@gmail.com>
2017-08-28 19:48:51 +0200

    Fixing styles on order detail

--- src/cemex/ClientApp/app/components/order-detail/order-detail.component.ts
+++ src/cemex/ClientApp/app/components/order-detail/order-detail.component.ts
@@ -3,40 +3,29 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
     selector: 'order-detail-page',
     templateUrl: './order-detail.component.html',
-    styleUrls:  ['./order-detail.component.scss',
-                 '../orders-table/orders-table.scss',
-                 '../orders-table/orders-table.specific.scss',
-                 '../order-builder/order-steps/summary/summary.step.scss'                      
-                ]
+    styleUrls: [
+        './order-detail.component.scss',
+        '../order-builder/order-steps/summary/summary.step.scss'
+    ]
 })
-export class OrderDetailComponent implements OnInit {
+export class OrderDetailComponent {
+    @Input() show: boolean = false;
+
     order = 7543189;
     requestDate = "31/12/2017, 15:00 - 16:00";
-    @Input() show: boolean = false;
-    constructor() { 
-    }
 
-    ngOnInit() {
-    }
+    constructor() { }
 
-
-    changeTabComments(){
+    changeTabComments() {
         document.getElementById("products").classList.remove('active');
-        document.getElementById("comments").classList.add('active');  
+        document.getElementById("comments").classList.add('active');
         this.show = true;
     }
-/* Waiting for invision    
-    changeTabAction(){
-    }     
-*/    
 
-    testTab(){
-        
-    }
-    changeTabProducts(){
+    changeTabProducts() {
         document.getElementById("comments").classList.remove('active');
-        document.getElementById("products").classList.add('active'); 
+        document.getElementById("products").classList.add('active');
         this.show = false;
-    } 
+    }
 
 }
