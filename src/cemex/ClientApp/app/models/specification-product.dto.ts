063e7751abaf846a668700eac3653ec61cea95b9
Rafael Briones <e-rbnunez@neoris.com>
2017-08-28 15:27:35 -0500

    adding read-mix shopping cart

--- src/cemex/ClientApp/app/models/specification-product.dto.ts
+++ src/cemex/ClientApp/app/models/specification-product.dto.ts
@@ -1,33 +1,36 @@
 import ProjectProfile from './projectProfile';
 
-export interface CementPackageSpecification {
+export interface IProductSpecification {
     productDescription: string;
     quantity: number;
     unit: string;
     requestDate: string;
     requestTime: string;
     productId: string;
-    maximumCapacity: number;
     contract: string;
-    payment: string;
     deliveryMode: string;
     location?: string;
     pointDelivery?: string;
     unitaryPrice: number;
+};
+
+
+export interface CementPackageSpecification extends IProductSpecification {
+    maximumCapacity: number;
+    payment: string;
 }
 
-export interface ReadymixSpecification {
-    productDescription: string;
-    quantity: number;
-    unit: string;
-    requestDate: string;
-    requestTime: string;
-    productId: string;
-    maximumCapacity: number;
-    contract: string;
-    payment: string;
-    deliveryMode: string;
+export interface ReadymixSpecification extends IProductSpecification {
     projectProfile: ProjectProfile;
+    dischargeTime: string;
+    transportMethod: string
+    pumpCapacityMin: string;
+    unloadType:string; 
+    pumpCapacityMax: string;
+    loadSize: string;
+    spacing: string;
+    kicker: boolean;
+
 }
 
 export interface CartProductGroup {
