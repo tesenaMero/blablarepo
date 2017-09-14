ffc43dfd054f99192a5f751e2983ec29f0c6e323
alexnbc <e-anegurita@neoris.com>
2017-09-14 15:39:53 +0200

    Added stepper new design UI

--- src/cemex/ClientApp/app/pipes/index.ts
+++ src/cemex/ClientApp/app/pipes/index.ts
@@ -1,3 +1,4 @@
 export { NoSpacePipe } from './nospace.pipe';
 export { ZeroPadPipe } from './zeropad.pipe';
 export { SumGroupProductPipe } from './cart.pipe';
+export { DatePipe } from './dateformat.pipe';
