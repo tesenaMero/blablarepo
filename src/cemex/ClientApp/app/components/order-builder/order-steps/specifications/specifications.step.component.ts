7d6ce3c5ebb8fd7664c592f905ae65031ba29671
leind <danielcardeenas@gmail.com>
2017-09-22 10:39:00 +0200

    Optional contract

--- src/cemex/ClientApp/app/components/order-builder/order-steps/specifications/specifications.step.component.ts
+++ src/cemex/ClientApp/app/components/order-builder/order-steps/specifications/specifications.step.component.ts
@@ -395,7 +395,7 @@ class PreProduct {
             let contracts = result.json().products;
             this.availableContracts = contracts;
 
-            if (contracts.length > 0) { this.contract = contracts[0]; this.loadings.contracts = false; }
+            if (contracts.length > 0) { this.loadings.contracts = false; }
             else { this.loadings.contracts = true; } // Disable it if no contracts
         });
     }
