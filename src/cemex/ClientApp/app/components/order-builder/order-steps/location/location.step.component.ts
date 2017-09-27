b73b8c0f10793e1a087075735af0f32d084bd6db
Denis Havranek <denis.havranek@neoris.net>
2017-09-27 15:40:56 +0200

    DQA-2388 Solves incorrect jobsite and POD preselection for orders when having one prefilled value

--- src/cemex/ClientApp/app/components/order-builder/order-steps/location/location.step.component.ts
+++ src/cemex/ClientApp/app/components/order-builder/order-steps/location/location.step.component.ts
@@ -266,7 +266,7 @@ export class LocationStepComponent implements OnInit, StepEventsListener {
                 location.name = location.shipmentLocationDesc;
             })
             this.loadings.locations = false;
-            if (this.locations.length > 0) {
+            if (this.locations.length === 1) {
                 this.jobsiteChanged(this.locations[0]);
                 this.locationIndex = 0;
             }
@@ -348,8 +348,8 @@ export class LocationStepComponent implements OnInit, StepEventsListener {
             });
 
             if (this.pods.length > 0) {
-                this.podsIndex = undefined;
-                this.manager.selectPointOfDelivery(undefined);
+                this.podsIndex = this.pods.length === 1 ? 0 : undefined;
+                this.manager.selectPointOfDelivery(this.pods.length === 1 ? this.pods[0] : undefined);
             }
 
             this.loadings.pods = false;
