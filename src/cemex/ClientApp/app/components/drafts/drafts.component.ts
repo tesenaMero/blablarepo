f33c58e1a1b01bfdb0bf61072adab5f56e7fa79f
Edwin Alejandro Esquivel Soto <edwin.esquivel@neoris.cxnetworks.net>
2017-10-04 16:12:19 +0200

    add country code and remove amount on draft for US customer

--- src/cemex/ClientApp/app/components/drafts/drafts.component.ts
+++ src/cemex/ClientApp/app/components/drafts/drafts.component.ts
@@ -13,14 +13,28 @@ export class DraftsComponent implements OnInit {
     rows: any[] = [];
     orders: any;
 
-    constructor(private ordersApi: OrdersApi, private t: TranslationService) {
-        // this.ordersApi.all('4169', 100).subscribe((response) => {
-        //     this.orders = response.json().orders.slice(0, 10);
-        //     console.log(this.orders);
-        //     this.initOrders();
-        // });
-        
-        this.initData();
+    constructor(private ordersApi: OrdersApi, private t: TranslationService) {                
+        this.ordersApi.all().subscribe((response) => {
+            if (response.status == 200) {
+                let orders: any[] = response.json().orders;
+                // Filter drafts
+                this.orders = orders.filter((item) => {
+                    if (item.status) {
+                        if (item.status.statusDesc)
+                            return item.status.statusCode === "DRFT";                    
+                    return true;
+                    }
+                });      
+                let countryCode = JSON.parse(sessionStorage.getItem('user_legal_entity'));                
+                // if Usa customer
+                if (countryCode && countryCode.countryCode.trim() === "US"){            
+                    this.initUsaCustomerOrders();
+                }   
+                else{
+                    this.initOrders();
+                }
+            }
+        });
     }
 
     ngOnInit() {
@@ -28,16 +42,43 @@ export class DraftsComponent implements OnInit {
 
     initOrders() {
         this.columns = [
+            { name: this.t.pt('views.table.order_no'), width: 15 },
+            { name: this.t.pt('views.table.submitted'), width: 15 },
+            { name: this.t.pt('views.table.location'), width: 25 },
+            { name: this.t.pt('views.table.pon'), width: 15 },
+            { name: this.t.pt('views.table.products'), width: 10 },
+            { name: this.t.pt('views.table.amount'), width: 10, sortable: false },
+            { name: this.t.pt('views.table.request_date'), width: 20 },
+            { name: this.t.pt('views.table.status'), width: 18 },
+            { name: this.t.pt('views.table.total'), width: 13 },
+        ]
+
+        this.orders.forEach((order) => {
+            this.rows.push([
+                { inner: order.orderCode, class: "order-id", title: true },
+                { inner: order.updatedDateTime, hideMobile: true },
+                { inner: order.jobsite.jobsiteCode + " " + order.jobsite.jobsiteDesc, subtitle: true },
+                { inner: order.purchaseOrder, hideMobile: true },
+                { inner: "<i class='cmx-icon-track'></i>", hideMobile: true },
+                { inner: order.totalQuantity + " tons" },
+                { inner: order.requestedDateTime },
+                { inner: "<span class='status " + order.status.statusDesc.toLowerCase() + "'></span>" + order.status.statusDesc, hideMobile: false },
+                { inner: "$" + order.totalAmount, class: "roboto-bold" },
+            ]);
+        });
+    }
+
+    // Usa customer without amount
+    initUsaCustomerOrders() {
+        this.columns = [
             //{ inner: '<i class="star cmx-icon-favourite-active" aria-hidden="true"></i>', width: 5 },
-            { name: "Order No", width: 15 },
-            { name: "Submitted", width: 15 },
-            { name: "Location", width: 25 },
-            { name: "Puchase Order Number", width: 15 },
-            { name: "Products", width: 10 },
-            { name: "Amount", width: 10, sortable: false },
-            { name: "Requested date", width: 20 },
-            { name: "Status", width: 18 },
-            { name: "Total amount", width: 13 },
+            { name: this.t.pt('views.table.order_no'), width: 15 },
+            { name: this.t.pt('views.table.submitted'), width: 15 },
+            { name: this.t.pt('views.table.location'), width: 25 },
+            { name: this.t.pt('views.table.pon'), width: 15 },
+            { name: this.t.pt('views.table.products'), width: 10 },
+            { name: this.t.pt('views.table.request_date'), width: 20 },
+            { name: this.t.pt('views.table.status'), width: 18 },
         ]
         
         this.orders.forEach((order) => {
@@ -48,49 +89,46 @@ export class DraftsComponent implements OnInit {
                 { inner: order.jobsite.jobsiteCode + " " + order.jobsite.jobsiteDesc, subtitle: true },
                 { inner: order.purchaseOrder, hideMobile: true },
                 { inner: "<i class='cmx-icon-track'></i>", hideMobile: true },
-                { inner: order.totalQuantity + " tons"},
                 { inner: order.requestedDateTime },
                 { inner: "<span class='status " + order.status.statusDesc.toLowerCase() + "'></span>" + order.status.statusDesc, hideMobile: false },
-                { inner: "$" + order.totalAmount, class: "roboto-bold" },
-                // { inner: "<span class='status " + order.status.statusDesc.toLowerCase() + "'></span>" + order.status.statusDesc, hideDesktop: true },
             ]);
         });
     }
 
-    initData() {
-        this.columns = [
-            //{ inner: '<i class="star cmx-icon-favourite-active" aria-hidden="true"></i>', width: 5 },
-            { name: "Saved", width: 10 },
-            { name: "Location", width: 10 },
-            { name: "Purchase Order Number", width: 10 },
-            { name: "Products", width: 10 },
-            { name: "Amount", width: 10 },
-            { name: "Requested date", width: 20 },
-            { name: "Total amount", width: 10, sortable: false },
-            { name: "", width: 5, sortable: false },
-        ]
+    // initData() {
+    //     this.columns = [
+    //         //{ inner: '<i class="star cmx-icon-favourite-active" aria-hidden="true"></i>', width: 5 },
+    //         { name: "Saved", width: 10 },
+    //         { name: "Location", width: 10 },
+    //         { name: "Purchase Order Number", width: 10 },
+    //         { name: "Products", width: 10 },
+    //         { name: "Amount", width: 10 },
+    //         { name: "Requested date", width: 20 },
+    //         { name: "Total amount", width: 10, sortable: false },
+    //         { name: "", width: 5, sortable: false },
+    //     ]
 
-        this.rows = [
-            [ 
-                { inner: "08/13/2017" }, 
-                { inner: "0864 Lonnie Parks" },
-                { inner: "7755-KJ120/00011" },
-                { inner: "<i class='cmx-icon-driver'></i>" },
-                { inner: "10 tons" },
-                { inner: "31/12/2017, 15:00 - 16:00" },
-                { inner: "$72394.99" },
-                { inner: "EDIT", class: "action-button" },
-            ],
-            [ 
-                { inner: "11/30/2017" }, 
-                { inner: "160 Kayleigh Tunnel" },
-                { inner: "1255-KJ120/00011" },
-                { inner: "<i class='cmx-icon-track'></i>" },
-                { inner: "11 tons" },
-                { inner: "31/12/2017, 15:00 - 16:00" },
-                { inner: "$35998.81", class: "roboto-bold" },
-                { inner: "EDIT", class: "action-button" },
-            ]
-        ]
-    }
+    //     this.rows = [
+    //         [ 
+    //             { inner: "08/13/2017" }, 
+    //             { inner: "0864 Lonnie Parks" },
+    //             { inner: "7755-KJ120/00011" },
+    //             { inner: "<i class='cmx-icon-driver'></i>" },
+    //             { inner: "10 tons" },
+    //             { inner: "31/12/2017, 15:00 - 16:00" },
+    //             { inner: "$72394.99" },
+    //             { inner: "EDIT", class: "action-button" },
+    //         ],
+    //         [ 
+    //             { inner: "11/30/2017" }, 
+    //             { inner: "160 Kayleigh Tunnel" },
+    //             { inner: "1255-KJ120/00011" },
+    //             { inner: "<i class='cmx-icon-track'></i>" },
+    //             { inner: "11 tons" },
+    //             { inner: "31/12/2017, 15:00 - 16:00" },
+    //             { inner: "$35998.81", class: "roboto-bold" },
+    //             { inner: "EDIT", class: "action-button" },
+    //         ]
+    //     ]
+    // }
 }
