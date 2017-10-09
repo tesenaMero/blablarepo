98d652814d39ed114b1dcb31b0c18f541fdafe96
Edwin Alejandro Esquivel Soto <edwin.esquivel@neoris.cxnetworks.net>
2017-10-09 17:46:52 +0200

    add validations on quantity

--- src/cemex/ClientApp/app/components/order-builder/order-steps/specifications/specifications.step.component.ts
+++ src/cemex/ClientApp/app/components/order-builder/order-steps/specifications/specifications.step.component.ts
@@ -25,6 +25,7 @@ let CircularJSON = require('circular-json');
 export class SpecificationsStepComponent implements StepEventsListener {
     @Output() initializeProductColorsEmitter = new EventEmitter<any>();
     @Output() onCompleted = new EventEmitter<any>();
+    @Output() quantityResult = new EventEmitter<number>();
 
     today: Date;
 
@@ -650,16 +651,19 @@ export class SpecificationsStepComponent implements StepEventsListener {
             if (contractBalance === undefined) {
                 if (isDelivery) {
                     if ((Validations.isBulkCement() || Validations.isCementBag()) && (conversion <= maxCapacitySalesArea)) {
+                        product.quantityGood();
                         return product.quantity = newQty;
                     }
                     else {
                         if (conversion <= maxCapacitySalesArea) {
+                            product.quantityGood();
                             return product.quantity = newQty;
                         }
                     }
                 }
                 else {
                     if (conversion <= maxCapacitySalesArea) {
+                        product.quantityGood();
                         return product.quantity = newQty;
                     }
                 }
@@ -671,16 +675,19 @@ export class SpecificationsStepComponent implements StepEventsListener {
                 }
                 else if (!isDelivery) {
                     if (conversion <= maxCapacitySalesArea) {
+                        product.quantityGood();
                         return product.quantity = newQty;
                     }
                 }
                 else {
                     if ((conversion <= maxCapacitySalesArea)) {
+                        product.quantityGood();
                         return product.quantity = newQty;
                     }
                 }
             }
             if (conversion <= maxCapacitySalesArea) {
+                product.quantityGood();
                 return product.quantity = newQty;
             }
 
@@ -690,27 +697,24 @@ export class SpecificationsStepComponent implements StepEventsListener {
             if (product.quantity <= 1 && toAdd < 0) { return; }
             if (product.quantity >= Number.MAX_SAFE_INTEGER && toAdd > 0) { return; }
             product.quantity += toAdd;
+            product.quantityGood();
         }
     }
 
     valuechange(product: PreProduct, newValue: number) {
-        let prodQuntity = newValue;
+        product.quantityBad();
+        if(newValue < 0 || newValue == null){       
+            return this.dashboard.alertError(this.t.pt('views.specifications.negative_amount'), 10000);
+        }
         let maxCapacitySalesArea = product.maximumCapacity;
         let conversion = product.convertToTons(newValue);
-
-        if(newValue < 0 || newValue == null){
-            newValue = 1;
-        }
-
         if (conversion > maxCapacitySalesArea) {
             product.quantityBad();
             return this.dashboard.alertError(this.t.pt('views.specifications.maximum_capacity_reached'), 10000);
         } else {
             product.quantityGood();
         }
-        
-        product.quantity = prodQuntity;
-        return product.quantity;
+        return product.quantity = newValue;
     }
 
     todayStr() {
