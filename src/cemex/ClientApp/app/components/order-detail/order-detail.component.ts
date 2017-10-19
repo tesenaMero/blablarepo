076f860a57420f522cf522bdc8f9a6da54139a3f
leind <danielcardeenas@gmail.com>
2017-10-19 15:44:56 +0200

    Show shipping source in detail only in usa pickup

--- src/cemex/ClientApp/app/components/order-detail/order-detail.component.ts
+++ src/cemex/ClientApp/app/components/order-detail/order-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { OrderDetailApi } from '../../shared/services/api/order-detail.service';
+import { OrderDetailApi, LegalEntityApi } from '../../shared/services/api';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TranslationService } from '@cemex-core/angular-services-v2/dist';
 import { DashboardService } from '../../shared/services/dashboard.service'
@@ -15,7 +15,7 @@ import { Validations } from '../../utils/validations';
         '../order-builder/order-steps/review/review.step.scss'
     ]
 })
-export class OrderDetailComponent {   
+export class OrderDetailComponent {
     //parameters
     orderDetailData: any;
     id: any;
@@ -33,74 +33,64 @@ export class OrderDetailComponent {
 
     countryCode: string;
 
-    constructor(private orderDetailApi: OrderDetailApi, private route: ActivatedRoute, private t: TranslationService, private dashboard: DashboardService, private router: Router) {
-        let userLegalEntity = JSON.parse(sessionStorage.getItem('user_legal_entity'));
-        this.countryCode = userLegalEntity.countryCode.trim();
-
+    constructor(private orderDetailApi: OrderDetailApi, private route: ActivatedRoute, private t: TranslationService, private dashboard: DashboardService, private router: Router, private legalEntityApi: LegalEntityApi) {
         this.sub = this.route.queryParams.subscribe(params => {
             if (params['orderId'] !== null) {
                 this.id = params['orderId'];
-            }        
+            }
             if (params['orderCode'] !== null) {
-                this.orderCode = params['orderCode'];                
+                this.orderCode = params['orderCode'];
             }
             if (params['country'] !== null) {
-                this.country = params['country'];                
-            }    
+                this.country = params['country'];
+            }
             if (params['businessLine'] !== null) {
-                this.businessLine = params['businessLine'];                
+                this.businessLine = params['businessLine'];
             }
 
             // New Logic
-            if (this.orderCode && this.orderCode.length > 0) {                
-                if (this.country && this.businessLine && this.country.trim() == "MX" && this.businessLine == "CEM"){
+            if (this.orderCode && this.orderCode.length > 0) {
+                if (this.country && this.businessLine && this.country.trim() == "MX" && this.businessLine == "CEM") {
                     this.type = 'ZTA';
-                    this.id = this.orderCode; 
+                    this.id = this.orderCode;
                 }
                 else {
                     this.type = 'SLS';
-                }    
+                }
             }
             else {
                 this.type = 'REQ';
             }
-        });        
-
-        orderDetailApi.byIdType(this.id, this.type).subscribe((response) => {
-            this.orderDetailData = response.json();
-            const parent = this.orderDetailData;
-            if (this.orderDetailData.salesArea.countryCode.trim() == "MX") { //Point Of Delivery
-                this.getJobsite();
-                this.getPod();
-            }
-            else {
-                if (this.orderDetailData.salesArea.countryCode.trim() == "US") { //Jobsite
-                    // console.log("In US Job");
-                    this.getJobsite(); 
-                }
-            }
-        }, error => {
-            this.dashboard.alertError("Couldn't retrieve the order");
-            this.router.navigate(['/ordersnproduct/app/orders']);
         });
-    } 
 
-    getJobsite() {
+        this.orderDetailApi.byIdType(this.id, this.type)
+        .flatMap((response) => {
+            this.orderDetailData = response.json();
+            this.fetchJobsite();
+            if (this.orderDetailData.salesArea.countryCode.trim() == "MX") { this.fetchPod(); }
+            return this.legalEntityApi.byCustomerId(this.orderDetailData.customer.customerId)
+        })
+        .subscribe((customer) => {
+            this.countryCode = customer.json().address.countryCode.trim();
+        });
+    }
+
+    fetchJobsite() {
         this.orderDetailApi.shipmentLocationsJob(this.orderDetailData.jobsite.jobsiteId).subscribe((response) => {
             this.jobsite = response.json();
             if (this.jobsite.shipmentLocations.length > 0) {
-                this.getStreetJobsite(this.jobsite.shipmentLocations[this.jobsite.shipmentLocations.length-1].address.addressId);
+                this.getStreetJobsite(this.jobsite.shipmentLocations[this.jobsite.shipmentLocations.length - 1].address.addressId);
             }
         });
     }
 
-    getPod() {
+    fetchPod() {
         this.orderDetailApi.shipmentLocationsPOD(this.orderDetailData.pointOfDelivery.pointOfDeliveryId).subscribe((response) => {
             this.pod = response.json();
             if (this.pod.shipmentLocations.length > 0) {
-                this.getStreetPOD(this.pod.shipmentLocations[this.pod.shipmentLocations.length-1].address.addressId);
+                this.getStreetPOD(this.pod.shipmentLocations[this.pod.shipmentLocations.length - 1].address.addressId);
             }
-        });          
+        });
     }
 
     getStreetJobsite(street) {
@@ -115,19 +105,26 @@ export class OrderDetailComponent {
         });
     }
 
-    showRequestTime(time){
-       return moment.utc(time).local().format('DD/MM/YYYY, HH:mm A');
+    showRequestTime(time) {
+        return moment.utc(time).local().format('DD/MM/YYYY, HH:mm A');
     }
 
-    showRegionDate(date){
-        if(this.countryCode === 'MX'){
+    showRegionDate(date) {
+        if (this.countryCode === 'MX') {
             return moment.utc(date).local().format('DD/MM/YYYY');
         } else {
             return moment.utc(date).local().format('MM/DD/YYYY');
         }
     }
 
+    // Pikcup && USA
+    shouldShowShippingSource() {
+        return this.countryCode == "US" && this.orderDetailData.shippingCondition.shippingConditionCode == "02";
+    }
+
     ngOnDestroy() {
-        this.sub.unsubscribe();
+        if (this.sub) {
+            this.sub.unsubscribe();
+        }
     }
 }
